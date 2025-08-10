0bc40cd955a40b2ab228e2f2e18d4d8cddc27302
Kirill <kirilldevelop1511@mail.ru>
2025-08-10 12:53:43 +0300

    Исправление ошибок

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -3,9 +3,22 @@ import { ReactNode, useEffect, useState } from 'react'
 import { ThemeContext } from './theme.context'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-	const [theme, setTheme] = useState(
-		() => localStorage.getItem('theme') || 'dark'
-	)
+	if (typeof window !== 'undefined') {
+	} else {
+		console.log('we are running on the server')
+	}
+	const [theme, setTheme] = useState(() => {
+		if (typeof window !== 'undefined') {
+			const storedItem = localStorage.getItem('theme')
+			if (storedItem) {
+				return storedItem
+			} else {
+				return 'dark'
+			}
+		} else {
+			return 'dark'
+		}
+	})
 	useEffect(() => {
 		document.documentElement.classList.toggle('dark', theme === 'dark')
 		localStorage.setItem('theme', theme)
